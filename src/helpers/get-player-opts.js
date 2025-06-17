90753596b115a92df0cefd41184ab205e7f34f64
Davlopez06 <davidlopezvaldes@gmail.com>
2025-06-17 11:36:03 -0500

    [FEATURE] Permutive in jwp

--- src/helpers/get-player-opts.js
+++ src/helpers/get-player-opts.js
@@ -31,28 +31,42 @@ function getPlayerOpts(opts) {
 
   if (hasAdvertising) {
     let playerSegsPerm = '';
+
     try {
       if (typeof window !== 'undefined') {
-        const segs = JSON.parse(localStorage.getItem('_pdfps') || '[]') || [];
+        const segs = JSON.parse(localStorage.getItem('_pdfps') || '[]');
         if (Array.isArray(segs)) {
           playerSegsPerm = segs.slice(0, 250).join(',');
         }
       }
     } catch (e) {
-      // eslint-disable-next-line no-console
       console.warn('No se pudieron leer los segmentos de Permutive:', e);
     }
 
+    const custParams = {
+      'video-id': '92347',
+      'video-channel': 'c-suite',
+      'video-campaign': '',
+      'video-series': '',
+      'video-category': 'it-management,internet-of-things,cio-role',
+      'video-tag': '',
+      devsite: 'false',
+      permutive: playerSegsPerm,
+    };
+
+    let custParamsStr = '';
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in custParams) {
+      if (custParamsStr !== '') custParamsStr += '&';
+      custParamsStr += `${encodeURIComponent(key)}=${encodeURIComponent(custParams[key])}`;
+    }
+
     const hasQuery = generatePrerollUrl.includes('?');
-    const encodedPermutive = `permutive=${encodeURIComponent(playerSegsPerm)}`;
-    const encodedCustParams = `cust_params=video-id%3D92347%26video-channel%3Dc-suite%26video-campaign%3D%26video-series%3D%26video-category%3Dit-management%2Cinternet-of-things%2Ccio-role%26video-tag%3D%26devsite%3Dfalse%26permutive%3D${encodeURIComponent(playerSegsPerm)}`;
-    const permutiveTagUrl = generatePrerollUrl + (hasQuery ? '&' : '?') + encodedPermutive;
-    const permutiveHasQuery = permutiveTagUrl.includes('?');
-    const finalTagUrl = permutiveTagUrl + (permutiveHasQuery ? '&' : '?') + encodedCustParams;
+    const finalTagUrl = `${generatePrerollUrl + (hasQuery ? '&' : '?')}cust_params=${encodeURIComponent(custParamsStr)}`;
 
     playerOpts.advertising = {
       client: 'googima',
-      admessage: 'Ad — xxs left',
+      admessage: 'Ad - xxs left',
       autoplayadsmuted: true,
       tag: finalTagUrl,
     };
